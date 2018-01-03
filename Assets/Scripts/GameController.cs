74e8b8a5b92542abe0d4ad572761187a5cfde66c
thisDelegExp <zguranaz@gmail.com>
2018-01-03 15:35:50 +0200

    Initial commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour {
+
+    public GameObject hazard;
+    public Vector3 spawnValues;
+    public int hazardCount;
+    public float spawnWait;
+    public float startWait;
+    public float waveWait;
+    public Text  scoreText;
+    public Text restartText;
+    public Text overText;
+    private bool gameOver;
+    private bool Restart;
+    private int score;
+
+    private void Update()
+    {
+        if (Restart)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+    IEnumerator SpawnWaves()
+    {
+        yield return new WaitForSeconds(startWait);
+        while (!gameOver)
+        {
+            for (int i = 0; i < hazardCount; i++)
+            {
+                Vector3 spawnPosition = new Vector3(Random.Range(-spawnValues.x, spawnValues.x), 0, spawnValues.z);
+                
+                Instantiate(hazard, spawnPosition, Quaternion.identity);
+                yield return new WaitForSeconds(spawnWait);
+            }
+            yield return new WaitForSeconds(waveWait);
+            if (gameOver)
+            {
+                restartText.text = "Press 'R' to restart";
+                Restart = true;
+                break;
+            }
+        }
+        
+   }
+    public void AddScore(int newScoreValue)
+    {
+        score += newScoreValue;
+        UpdateScore();
+    }
+    void Start()
+    {
+        gameOver = false;
+        Restart = false;
+        restartText.text = "";
+        overText.text = "";
+        score = 0;
+        UpdateScore();
+        StartCoroutine(SpawnWaves());
+    }
+
+    public void GameOver()
+    {
+        overText.text = "GAME OVER";
+        gameOver = true;
+    }
+
+    void UpdateScore()
+    {
+        scoreText.text = "Score: " + score; 
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Boundary boundary;
+    public float tilt;
+    public GameObject shot;
+    public Transform[] turrets;
+    public float fireRate;
+    private float nextFire = 0.0f;
+    void FixedUpdate()
+    { 
+        
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        GetComponent<Rigidbody>().velocity = new Vector3(moveHorizontal, 0.0f, moveVertical)*speed;
+        GetComponent<Rigidbody>().position = new Vector3
+            (
+                Mathf.Clamp(GetComponent<Rigidbody>().position.x,boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(GetComponent<Rigidbody>().position.z, boundary.zMin, boundary.zMax)
+            );
+        GetComponent<Rigidbody>().rotation = Quaternion.Euler(GetComponent<Rigidbody>().velocity.z * tilt, 0.0f, GetComponent<Rigidbody>().velocity.x*-tilt);
+    }
+
+     void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            foreach (Transform gun in turrets)
+            {
+
+                Instantiate(shot, gun.position, Quaternion.Euler(0f,gun.rotation.y,0f));
+            }
+            GetComponent<AudioSource>().Play();
+        }
+    }
+
+
+}
+[System.Serializable]
+public struct Boundary
+{
+    public float xMin, xMax, zMin, zMax;
+}
