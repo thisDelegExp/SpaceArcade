31d31f47da054af3331eb028d55dfeb9f00946fc
thisDelegExp <zguranaz@gmail.com>
2018-01-04 13:16:06 +0200

    +Scrollable background, --various fixes

---
+++ Assets/Scripts/BGScroller.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGScroller : MonoBehaviour
+{
+    public float scrollSpeed;
+    public float tileZ;
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+    void Update ()
+    {
+        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileZ);
+        transform.position = startPosition + Vector3.forward * newPosition;
+	}
+}
