f01fe10f03e690d118c59145c4885d835a843ddf
thisDelegExp <zguranaz@gmail.com>
2018-01-04 16:23:46 +0200

    +Enemies

--- Assets/Scripts/DestroyByContact.cs
+++ Assets/Scripts/DestroyByContact.cs
@@ -16,8 +16,8 @@ public class DestroyByContact : MonoBehaviour {
     }
     void OnTriggerEnter(Collider other)
     {
-        if(other.tag == "Boundary") return;
-        Instantiate(explosion, transform.position, transform.rotation);
+        if(other.tag == "Boundary" || other.tag=="Enemy") return;
+        if(explosion!=null) Instantiate(explosion, transform.position, transform.rotation);
         if (other.tag == "Player")
         {
             Instantiate(playerExplosion, other.transform.position, other.transform.rotation);
---
+++ Assets/Scripts/EvasiveManeuver.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EvasiveManeuver : MonoBehaviour
+{
+    public Vector2 startWait;
+    public float dodge;
+    public Vector2 maneuverTime;
+    public Vector2 maneuverWait;
+    private float targetManeuver;
+    private Rigidbody rb;
+    public float smoothing;
+    private float currentSpeed;
+    public Boundary boundary;
+    public float tilt;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        currentSpeed = rb.velocity.z;
+        StartCoroutine(Evade());
+    }
+    IEnumerator Evade()
+    {
+        yield return new WaitForSeconds(Random.Range(startWait.x, startWait.y));
+        while (true)
+        {
+            targetManeuver = Random.Range(1,dodge)*-Mathf.Sign(transform.position.x);
+            yield return new WaitForSeconds(Random.Range(maneuverTime.x,maneuverTime.y));
+            targetManeuver = 0;
+            yield return new WaitForSeconds(Random.Range(maneuverWait.x,maneuverWait.y));
+        }
+
+    }
+
+    private void FixedUpdate()
+    {
+        float newManeuver = Mathf.MoveTowards(rb.velocity.x, targetManeuver, Time.deltaTime*smoothing);
+        rb.velocity = new Vector3(newManeuver, 0f, currentSpeed);
+        rb.position = new Vector3
+            (
+                Mathf.Clamp(rb.position.x, boundary.xMin, boundary.xMax),
+                0.0f,
+                Mathf.Clamp(rb.position.z, boundary.zMin, boundary.zMax)
+            );
+        rb.rotation = Quaternion.Euler(rb.velocity.z * tilt, 0.0f, rb.velocity.x * -tilt);
+    }
+
+}
